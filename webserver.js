03c4a30b7d15a324d02551c8ac3615983516a90d
altanai <tara181989@gmail.com>
2020-07-19 22:54:05 +0530

    added

--- webserver.js
+++ webserver.js
@@ -67,6 +67,25 @@ console.log("< ------------------------ HTTPS Server -------------------> ");
 console.log(" Web server env => " + properties.enviornment + " running at " + properties.http2Port);
 
 
-// console.log("< ------------------------ REST API Server -------------------> ");
-// var _restapi = require('./node_modules/webrtcdevelopment/build/webrtcdevelopmentServer.js').restapi;
-// var restapi = _restapi(realtimecomm, options, app, properties);
+var _realtimecomm = require('./node_modules/webrtcdevelopment/build/webrtcdevelopmentServer.js').realtimecomm;
+var realtimecomm = _realtimecomm(properties, options, null, (socket) => {
+    try {
+        var params = socket.handshake.query;
+
+        if (!params.socketCustomEvent) {
+            params.socketCustomEvent = 'custom-message';
+        }
+
+        socket.on(params.socketCustomEvent, function (message) {
+            try {
+                socket.broadcast.emit(params.socketCustomEvent, message);
+            } catch (e) {
+                // Raven.captureException(e);
+                console.error(e);
+            }
+        });
+    } catch (e) {
+        // Raven.captureException(e);
+        console.error(e);
+    }
+});
